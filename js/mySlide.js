209880f135166d8a0f83764cb6ce94402507379b
Tboller <tboller@hawk.iit.edu>
2018-04-18 15:47:22 -0500

    added changes to css and js to make slider more visually appealing

--- js/mySlide.js
+++ js/mySlide.js
@@ -1,3 +1,9 @@
     $(document).ready(function(){
-      $('.slider').bxSlider();
+          $('.slider').bxSlider({
+      mode: 'fade',
+      captions: true,
+      slideWidth: 800,
+	  adaptiveHeight: true
+      });
     });
+	
